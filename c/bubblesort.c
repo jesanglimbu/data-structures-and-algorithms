f859a2b76e1f3c5887805a79e2a63a395cc11d02
jesang-limbu <limbujesang@gmail.com>
2026-02-20 00:19:26 +0000

    build: finish bubble sort implementation
    Add bubblesort.c and finish algorithm.

---
+++ c/bubblesort.c
@@ -0,0 +1,21 @@
+#include "bubblesort.h"
+#include <stdbool.h>
+
+void bubble_sort(int arr[], size_t len) {
+	size_t i = 0;
+	bool swapped = true;
+
+	while (swapped && i < len) {
+		swapped = false;
+		for (size_t j = 0; j < len-1; j++) {
+			if (arr[j] > arr[j+1]) {
+				int tmp = arr[j+1];
+				arr[j+1] = arr[j];
+				arr[j] = tmp;
+				swapped = true;
+			}
+		}
+		i++;
+	}
+			
+}
