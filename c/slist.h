982b6941b647958702028006ad8a332c737b594a
jesang-limbu <limbujesang@gmail.com>
2026-03-02 21:36:37 +0000

    build: make slist.h
    Add methods that will be implemented later on.

---
+++ c/slist.h
@@ -0,0 +1,24 @@
+#ifndef _SLIST_H_
+#define _SLIST_H_
+
+#include "snode.h"
+#include <stdbool.h>
+
+typedef struct snode node;
+
+struct slist {
+	node *start;
+	node *end;
+	int size;
+};
+
+struct slist *init_slist();
+bool is_empty(struct slist *l);
+int get_size(struct slist *l);
+int get_node(struct slist *l, int val);
+void append(struct slist *l, int val);
+void delete(struct slist *l, int i);
+void insert(struct slist *l, int i, int val);
+void print_list(struct slist *l);
+
+#endif
