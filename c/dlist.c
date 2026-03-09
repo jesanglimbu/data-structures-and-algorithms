6c7a00dd72900ab0e24c9167154ebdfa75e19ded
jesanglimbu <limbujesang@gmail.com>
2026-03-09 12:26:07 +0000

    src: implement insert on dlist.c

--- c/dlist.c
+++ c/dlist.c
@@ -16,3 +16,34 @@ list *init_dlist()
 	return l;
 }
 
+void insert(list *l int val, int index)
+{
+	if (index >= l->size || index < 0) {
+		printf("Error: tried to insert a node in an out of range index!\n");
+		return;
+	} else {
+		node *n = malloc(sizeof(node));
+		n->val = val;
+		
+		// if the index is the last index
+		if (index == (l->size)-1) {
+			n->prev = l->end;
+			l->end->next = n;
+			l->end = n;
+		}
+		if (index == 0) {
+			n->next = l->start;
+			l->start->prev = n;
+			l->start = n;
+		} else {
+			node *prev_node = get_node(l, index);
+			node *next_node = next_node->next;
+			n->prev = prev_node;
+			n->next = next_node;
+			prev_node->next = n;
+			next_node->prev = n;
+		}
+
+		(l->size)++;
+	}
+}
