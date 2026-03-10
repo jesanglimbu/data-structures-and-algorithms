3a04c90275bc8494b1c4a2e1127a41eb8cd8eeb2
jesanglimbu <limbujesang@gmail.com>
2026-03-10 11:21:53 +0000

    src: add methods in dlist.c and fix naming issues
    New:
    	- prepend_dlist method
    	- append_dlist method
    	- delete_dlist method
    	- clear_dlist method
    	- get_dnode method
    	- print_dlist method
    
    Changes:
    	- Method and typedef names (so that it doesn't clash with
    	slist)

--- c/dlist.c
+++ c/dlist.c
@@ -16,28 +16,24 @@ list *init_dlist()
 	return l;
 }
 
-void insert(list *l int val, int index)
+void insert_dlist(list *l, int val, int index)
 {
 	if (index >= l->size || index < 0) {
 		printf("Error: tried to insert a node in an out of range index!\n");
 		return;
 	} else {
-		node *n = malloc(sizeof(node));
+		dnode *n = malloc(sizeof(dnode));
 		n->val = val;
 		
 		// if the index is the last index
 		if (index == (l->size)-1) {
-			n->prev = l->end;
-			l->end->next = n;
-			l->end = n;
+			append_dlist(l, val);
 		}
 		if (index == 0) {
-			n->next = l->start;
-			l->start->prev = n;
-			l->start = n;
+			prepend_dlist(l, val);
 		} else {
-			node *prev_node = get_node(l, index);
-			node *next_node = next_node->next;
+			dnode *prev_node = get_dnode(l, index);
+			dnode *next_node = prev_node->next;
 			n->prev = prev_node;
 			n->next = next_node;
 			prev_node->next = n;
@@ -45,5 +41,105 @@ void insert(list *l int val, int index)
 		}
 
 		(l->size)++;
+	}
+}
+
+void prepend_dlist(list *l, int val)
+{
+	dnode *n = malloc(sizeof(dnode));
+	n->val = val;
+	n->next = l->start;
+	l->start->prev = n;
+	l->start = n;
+	(l->size)++;
+}
+
+void append_dlist(list *l, int val)
+{
+	dnode *n = malloc(sizeof(dnode));
+	n->val = val;
+	if (l->size == 0) { // if list has no nodes, we make the node the starting node
+		l->start = n;
+		l->end = n;
+	} else {
+		n->prev = l->end;
+		l->end->next = n;
+		l->end = n;
+	}
+	(l->size)++;
+}
+
+void delete_dlist(list *l, int index)
+{
+	if (index < 0 || index >= l->size) {
+		printf("Error: tried to delete a node in an index out of range!\n");
+	} else {
+		if (index == 0) {
+			l->start = l->start->next;
+			free(l->start->prev);
+		} else if (index == (l->size)-1) {
+			l->end = l->end->prev;
+			free(l->end->next);
+		} else {
+			dnode *n = get_dnode(l, index);
+			dnode *next_node = n->next;
+			dnode *prev_node = n->prev;
+			free(n);
+			prev_node->next = next_node;
+			next_node->prev = prev_node;
+		}
+		(l->size)--;
+	}
+	
+}
+
+int search_dlist(list *l, int val)
+{
+	int len = l->size;
+	dnode *n = l->start;
+
+	for (int i = 0; i < len; i++) {
+		if (n->val == val) {
+			return i;
+		} else {
+			n = n->next;
+		}
+	}
+
+	return -1;
+}
+
+dnode *get_dnode(list *l, int index)
+{
+	if (index >= 0 && index < l->size) {
+		dnode *n = l->start;
+		for (int i = 0; i < index; i++) {
+			n = n->next;
+		}
+
+		return n;
+	}
+
+	return null;
+}
+
+void clear_dlist(list *l)
+{
+	free(l);
+	l = init_dlist();
+}
+
+
+void print_dlist(list *l)
+{
+	if (l->size == 0) {
+		printf("No elements in the list!\n");
+	} else {
+		dnode *n = l->start;
+		for (int i = 0; i < l->size; i++) {
+			printf("%d ", n->val);
+			n = n->next;
+		}
+		printf("\n");
 	}
 }
--- c/dlist.h
+++ c/dlist.h
@@ -1,8 +1,6 @@
 #ifndef _DLIST_H_
 #define _DLIST_H_
 
-#include <stdbool.h>
-
 typedef struct dnode {
 	int val;
 	struct dnode *next;
@@ -16,10 +14,11 @@ struct dlist {
 };
 
 struct dlist *init_dlist();
-bool dlist_is_empty(struct dlist *l);
 void insert_dlist(struct dlist *l, int i, int val);
 void append_dlist(struct dlist *l, int val);
-void delete_dlist(struct dlist *l);
+void prepend_dlist(struct dlist *l, int val);
+void delete_dlist(struct dlist *l, int index);
+void clear_dlist(struct dlist *l);
 int search_dlist(struct dlist *l, int val);
 dnode *get_dnode(struct dlist *l, int i);
 struct dlist *concat_dlist(struct dlist *l1, struct dlist *l2);
