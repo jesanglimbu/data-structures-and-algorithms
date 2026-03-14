bd107f4c9d534134ffadd45399bfc450c5cd4521
jesanglimbu <limbujesang@gmail.com>
2026-03-14 18:06:36 +0000

    src: add enqueue, dequeue and print_queue method

--- src/queue.c
+++ src/queue.c
@@ -1,6 +1,7 @@
 #include "queue.h"
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 struct queue
 *init_queue()
@@ -21,11 +22,40 @@ enqueue(struct queue *q, int val)
 	t->next = NULL;
 
 	if (q->size == 0) {
-		q->start = t;
+		q->first = t;
+		q->last = t;
 	} else {
-		q->end->next = t;
-		q->end = t;
+		q->last->next = t;
+		q->last = t;
 	}
 
 	q->size++;
+}
+
+int
+dequeue(struct queue *q)
+{
+	struct task *current_task = q->first;
+	int val = current_task->val;
+	q->first = current_task->next;
+	free(current_task);
+
+	q->size--;
+	
+	return val;
+}
+
+void
+print_queue(struct queue *q)
+{
+	if (q->size == 0) {
+		printf("No tasks in the queue!\n");
+	} else {
+		struct task *t = q->first;
+		for (int i = 0; i < q->size; i++) {
+			printf("%d ", t->val);
+			t = t->next;
+		}
+		printf("\n");
+	}
 }
