c1277e74244e9981b2aa8c91b142e27cba4e4a2c
jesang-limbu <limbujesang@gmail.com>
2026-03-17 19:54:43 +0000

    build: add heap.h and heap.c
    Implemented some methods. Incomplete and may not work.

---
+++ include/heap.h
@@ -0,0 +1,18 @@
+#ifndef _HEAP_H_
+#define _HEAP_H_
+
+typedef struct heap heap_t;
+
+heap_t *heap_init();
+
+void heap_insert(heap_t *h, int val);
+
+int heap_max(heap_t *h);
+
+int heap_delete_max(heap_t *h);
+
+int heap_height(heap_t *h);
+
+int heap_size(heap_t *h);
+
+#endif
---
+++ src/heap.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+#include "heap.h"
+#include "queue.h"
+
+struct heap_node {
+	int val;
+	struct heap_node *parent;
+	struct heap_node *lchild;
+	struct heap_node *rchild;
+};
+
+struct heap {
+	struct heap_node *root;
+	int size;
+	int height;
+};
+
+struct heap
+*heap_init()
+{
+	struct heap *my_heap = malloc(sizeof(struct heap));
+	my_heap->root = NULL;
+	my_heap->size = 0;
+
+	return my_heap;
+}
+
+int
+heap_height(struct heap *h)
+{
+	return h->height;
+}
+
+int
+heap_size(struct heap *h)
+{
+	return h->size;
+}
+
+void
+heap_insert(struct heap *h, int val)
+{
+	struct heap_node *n = malloc(sizeof(struct heap_node));
+	n->val = val;
+	n->parent = NULL;
+	n->lchild = NULL;
+	n->rchild = NULL;
+	
+	
+	struct heap_node *m = h->root;
+
+	if (h->height == -1) { 	
+		h->root = m;
+		h->height++;
+	} else { /* Find the next position for the leaf. */
+		int height = 0;
+		while (m->lchild != NULL) {
+			m = m->rchild;
+			height++;
+		}
+
+		m->lchild = n;
+		n->parent = m;
+		
+		if (height > h->height)
+			h->height++;
+	}
+
+	h->size++;
+}
+
