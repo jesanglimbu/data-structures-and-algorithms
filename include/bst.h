34e0cd47745ed21d0255ce544566e138ea3c93e5
jesang-limbu <limbujesang@gmail.com>
2026-04-04 18:07:11 +0100

    src(include/bst.h): modify and delete some methods
    - Remove bst_node_t since node only has int val
    - Change return type on search methods

--- include/bst.h
+++ include/bst.h
@@ -1,21 +1,16 @@
 #ifndef _BST_H_
 #define _BST_H_
 
-#include <stdbool.h>
-
 typedef struct bst bst_t;
-typedef struct bst_node bst_node_t;
 
 bst_t *bst_init();
 
 void bst_insert(bst_t *t, int val);
 
-void bst_destroy(bst_t *t);
+int bst_destroy(bst_t *t);
 
-bst_node_t *bst_root(bst_t *t);
+int bst_dfs(bst_t *t, int val);
 
-bool bst_dfs(bst_t *t, bst_node_t *n, int val);
-
-bool bst_bfs(bst_t *t, bst_node_t *n, int val);
+int bst_bfs(bst_t *t, int val);
 
 #endif
